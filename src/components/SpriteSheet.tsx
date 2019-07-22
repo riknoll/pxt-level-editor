3e91e9722af5c9fc3ee4bd600d718d713a52b166
Jacob Bovee <boveejacob@gmail.com>
2019-07-22 15:16:51 -0700

    Sprite Sheet component

---
+++ src/components/SpriteSheet.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { loadImageAsync, ClientCoordinates } from '../util';
+import { TileSet } from '../tileset';
+
+interface SpriteSheetProps {
+    src: string;
+    index: number;
+    size?: number;
+    alt?: string;
+}
+
+interface SpriteSheetState {
+    coords?: ClientCoordinates;
+}
+
+export default class SpriteSheet extends React.Component<SpriteSheetProps, SpriteSheetState> {
+    private tileSet: TileSet;
+
+    constructor(props: SpriteSheetProps) {
+        super(props);
+        this.state = {};
+
+        this.loadTileSet = this.loadTileSet.bind(this);
+        this.loadTileSet();
+    }
+
+    private loadTileSet() {
+        const { src, size, index } = this.props;
+
+        loadImageAsync(src)
+            .then((el) => {
+                this.setState({ coords: new TileSet(el, size).indexToCoord(index) });
+            });
+    }
+
+    public render() {
+        const { src, alt, index, size } = this.props;
+        const { coords } = this.state;
+
+        return (
+            <img
+                alt={alt || `tile-${index}`}
+                ref={'spriteSheetCanvas'}
+                src={src}
+                style={{
+                    width: size,
+                    height: size,
+                    objectFit: 'none',
+                    objectPosition: coords && `-${coords.clientX}px -${coords.clientY}px`
+                }}
+            />
+        );
+    }
+}
