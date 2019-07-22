7d3796a6a024a5f5815dd9b59fe1e364d741d1ed
Jeanie DuMont <jeandu@microsoft.com>
2019-07-22 14:14:24 -0700

    added EditingTools component and editButton for erase, pointer, and pencil.

---
+++ src/components/EditingTools.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import '../css/editingTools.css';
+import { EditButton } from './EditButton';
+
+export class EditingTools extends React.Component<{}, {}> {
+
+    constructor(props: {}) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="editingTools">
+               Editing Tools component 
+                <EditButton className="fas fa-eraser" onClick={() => { console.log("erase button clicked"); } } id="erase"   />
+                <EditButton className="fas fa-pencil-alt" onClick={() => { console.log("pencil button clicked"); }} id="pencil"   />
+                <EditButton className="fas fa-mouse-pointer" onClick={() => { console.log("pointer button clicked"); }} id="pointer" />
+
+            </div>
+           
+        );
+    }
+}
