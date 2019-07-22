0e3a07b5925a826abafc1e8f299c19a1034efacf
Jacob Bovee <boveejacob@gmail.com>
2019-07-22 16:56:29 -0700

    Interactables and spawners

--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -3,7 +3,8 @@ import { Panel } from './Panel';
 import '../css/toolbox.css';
 import { ToolboxTerrainPanel } from './Toolbox/ToolboxTerrainPanel';
 import { ToolboxItemPanel } from './Toolbox/ToolboxItemPanel';
-
+import { ToolboxSpawnersPanel } from './Toolbox/ToolboxSpawnersPanel';
+import { ToolboxInteractablesPanel } from './Toolbox/ToolBoxInteractablesPanel';
 export class Toolbox extends React.Component<{}, {}> {
 
     constructor(props: {}) {
@@ -17,6 +18,8 @@ export class Toolbox extends React.Component<{}, {}> {
                     <h2>Toolbox component</h2>
                     <ToolboxTerrainPanel></ToolboxTerrainPanel>
                     <ToolboxItemPanel></ToolboxItemPanel>
+                    <ToolboxSpawnersPanel></ToolboxSpawnersPanel>
+                    <ToolboxInteractablesPanel />
                 </div>
             </div>
         );
