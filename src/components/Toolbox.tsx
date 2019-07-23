227851e711bd34f315f0f8157de9aa8eff885493
Abhijith Chatra <abchatra@microsoft.com>
2019-07-23 12:28:58 -0700

    Version v1 of the sprite editor

--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import '../css/toolbox.css';
+import { SpriteEditorButton } from './SpriteEditorButton';
+
+
 
 export class Toolbox extends React.Component<{}, {}> {
 
@@ -7,10 +10,14 @@ export class Toolbox extends React.Component<{}, {}> {
         super(props);
     }
 
+    onChange = (v: string) => {
+        console.log(v);
+    }
     render() {
         return (
             <div className="toolbox">
                 Toolbox component
+                <SpriteEditorButton onChange={this.onChange} />
             </div>
         );
     }
