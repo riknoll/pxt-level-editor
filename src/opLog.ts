5292d50989aa48e00b9fb4fc26e2604b29c7d1f9
darzu <daryl.zuniga@gmail.com>
2019-07-24 18:33:05 -0700

    work in progress on snapshots

--- src/opLog.ts
+++ src/opLog.ts
@@ -1,22 +1,53 @@
+class RingBuffer<T> {
+    private data: T[] = []
+    private next: number = 0
 
+    constructor(public size: number) { }
 
+    private wrap(idx: number, lim: number) {
+        while (idx >= lim)
+            idx -= lim
+        return idx
+    }
+
+    add(t: T) {
+        this.data[this.next] = t
+
+        this.next = this.wrap(this.next + 1, this.size)
+    }
+
+    *[Symbol.iterator]() {
+        if (!this.data.length)
+            return
+        let itr = this.wrap(this.next, this.data.length)
+        for (let n = 0; n < this.data.length; n++) {
+            yield this.data[itr]
+            itr = this.wrap(itr + 1, this.data.length)
+        }
+    }
+
+}
 
 export class OperationLog<State extends ReadonlyState, ReadonlyState, Operation> {
     private log: Operation[] = []
     private cursor: number = -1
     private currState: State;
     private changeListeners: ((newState?: State) => void)[] = [];
-
-    private snapshots: { cursor: number, state: State }[] // TODO: ringbuffer
+    private snapshots = new RingBuffer<{ idx: number | null, state: State }>(5);
 
     constructor(private newState: () => State, private applyOperation: (old: State, op: Operation) => State) {
         this.currState = newState()
     }
 
     private truncate() {
-        // DESTRUCTIVE
+        // DESTRUCTIVE. Remove/invalidate all state after the cursor (exclusive), including snapshots
         if (this.cursor < this.log.length)
             this.log.splice(this.cursor + 1)
+        for (let s of this.snapshots) {
+            if (this.cursor < s.idx) {
+                s.idx = null
+            }
+        }
     }
     private lastIdx(): number {
         return this.log.length - 1
@@ -48,18 +79,39 @@ export class OperationLog<State extends ReadonlyState, ReadonlyState, Operation>
         }
         this.log.push(op)
         this.cursor = this.lastIdx()
-
         this.currState = this.applyOperation(this.currState, op)
 
+        // TODO(dz): take snapshot
+
         this.onChange()
+    }
+
+    private lastSnapshot(): { idx: number | null, state: State } {
+        let c: { idx: number | null, state: State } = null
+        for (let s of this.snapshots) {
+            if (s.idx && s.idx <= this.cursor)
+                if (!c || c.idx < s.idx)
+                    c = s
+        }
+        return c
     }
 
     undo(): void {
         if (this.cursor >= 0)
             this.cursor--
 
-        // TODO(dz): incremental undo
-        let newState = this.computeState(this.applyOperation, this.newState());
+        // incremental undo by working from the last snapshot
+        // TODO(dz): if there aren't any snapshots, rebuild them
+        let start = this.lastSnapshot()
+        if (start)
+            start = start.clone()
+        if (!start)
+            start = { idx: 0, state: this.newState() }
+
+        let newState = this.log
+            .slice(start.idx, this.cursor + 1)
+            .reduce(this.applyOperation, start.state)
+
         this.currState = newState
 
         this.onChange()
@@ -73,9 +125,5 @@ export class OperationLog<State extends ReadonlyState, ReadonlyState, Operation>
         this.currState = this.applyOperation(this.currState, op)
 
         this.onChange()
-    }
-
-    private computeState(reduceFn: (prevState: State, nextOp: Operation) => State, defState: State): State {
-        return this.log.slice(0, this.cursor + 1).reduce(reduceFn, defState)
     }
 }
